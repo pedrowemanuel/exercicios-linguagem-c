5945e6a6ad1922e83e8ef04f13a521ef2ee1cb12
Pedro Emanuel <pedronuel36@gmail.com>
2020-12-13 15:17:18 +0000

    atividade aula 02 e 04 do 12

--- atividades/aula_02_e_0412/ex07/main.c
+++ atividades/aula_02_e_0412/ex07/main.c
@@ -4,8 +4,8 @@ int main() {
 
   unsigned char valor1 = 16; // 10000
   unsigned char valor2 = 19; // 10011
-                             // 00011
-  valor1 = valor1 ^ valor2;
+                             
+  valor1 = valor1 ^ valor2; // 00011
 
   printf("RESULTADO: %d \n", valor1);
 
